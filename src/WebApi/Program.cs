9261a7e9e17a033bd73e5e96a078f28cb48063ef
Renato Leite <hr2.renato@gmail.com>
2023-08-27 14:12:27 -0300

    Adding docker file and adding app to docker compose

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -13,12 +13,8 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseAuthentication();
